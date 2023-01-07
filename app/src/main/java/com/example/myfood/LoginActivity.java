8929bed9dde1ed64d373d3ba28c6a4df13637ae9
ANASS ABBOU <99830093+anassabbou@users.noreply.github.com>
2023-01-07 05:55:23 +0100

    register of client it's work, edit design

--- app/src/main/java/com/example/myfood/LoginActivity.java
+++ app/src/main/java/com/example/myfood/LoginActivity.java
@@ -2,13 +2,20 @@ package com.example.myfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    public TextView forgetPass,CreateAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        //CreateAccount=(TextView) findViewById(R.id.CrtAccount);
+
+
+
 
 
             }
